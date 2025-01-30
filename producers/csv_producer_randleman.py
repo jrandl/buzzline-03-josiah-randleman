68fa56da8ee5ffcb00a2e95e38ae8d7bea36ae49
Josiah Randleman <jrandl516@gmail.com>
2025-01-29 22:13:46 -0600

    added custom message to unique kafka csv producer

--- producers/csv_producer_randleman.py
+++ producers/csv_producer_randleman.py
@@ -70,7 +70,7 @@ DATA_FOLDER = PROJECT_ROOT.joinpath("data")
 logger.info(f"Data folder: {DATA_FOLDER}")
 
 # Set the name of the data file
-DATA_FILE = DATA_FOLDER.joinpath("smoker_temps.csv")
+DATA_FILE = DATA_FOLDER.joinpath("smoker_temps_randleman.csv")
 logger.info(f"Data file: {DATA_FILE}")
 
 #####################################
